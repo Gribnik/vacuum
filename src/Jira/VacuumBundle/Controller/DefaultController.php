a50c80ebd5913f5314c1173635276c278cd5c17b
enarc <enarc@atwix.com>
2013-11-06 11:58:29 +0200

    Missing files

---
+++ src/Jira/VacuumBundle/Controller/DefaultController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Jira\VacuumBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Jira\VacuumBundle\Entity\Login;
+//use Symfony\Component\BrowserKit\Response;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return new Response('Hello world');
+    }
+
+    public function loginAction()
+    {
+        $login = new Login();
+        $form = $this->createFormBuilder($login)
+            ->setAction($this->generateUrl('login'))
+            ->add('login', 'text')
+            ->add('password', 'text')
+            ->add('go', 'submit')
+            ->getForm();
+        return $this->render('JiraVacuumBundle:Login:login.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+    public function loginProcessAction(Request $request)
+    {
+        $login = new Login();
+
+        $form = $this->createFormBuilder($login)
+            ->add('login', 'text')
+            ->add('password', 'text')
+            ->add('go', 'submit')
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            return $this->redirect($this->generateUrl('index'));
+        } else {
+            return $this->render('JiraVacuumBundle:Login:login.html.twig', array(
+                'form' => $form->createView()
+            ));
+        }
+    }
+}
---
+++ src/Jira/VacuumBundle/JiraVacuumBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Jira\VacuumBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class JiraVacuumBundle extends Bundle
+{
+}
